40860b4baea8b0696525fd628c65fbc7e3638f71
Gabriel <gabrielvalino25@hotmail.com>
2025-07-08 20:50:31 -0300

    fix(cors): update origin from https to http in Fastify config

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.register(fastifyCors, { origin: 'https://localhost:5173' })
+app.register(fastifyCors, { origin: 'http://localhost:5173' })
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
